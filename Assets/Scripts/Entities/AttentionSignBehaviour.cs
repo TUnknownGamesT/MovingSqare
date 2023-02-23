c97f06ddd606423e93d1a05f23cbe8002170a683
Daniel Bogatu <daniel.bogatu@amplified.software>
2023-02-24 01:48:30 +0200

    Fixing Deploy bugs

--- Assets/Scripts/Entities/AttentionSignBehaviour.cs
+++ Assets/Scripts/Entities/AttentionSignBehaviour.cs
@@ -14,6 +14,7 @@ public class AttentionSignBehaviour :MonoBehaviour, IMoveAlongAnAxe
     
     private void Start()
     {
+        Destroy(gameObject,7);
         localTransform = GetComponent<Transform>();
     }
 
